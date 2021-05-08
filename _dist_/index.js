c8f5de2160a5ed5ab4eca8ea6808a46fa84364cf
JoseGarcia2001 <JoseGarcia2001@users.noreply.github.com>
2021-05-08 01:33:22 +0000

    deploy: 009bbd8fdcf9e9c50fdec04914697e452b1d2f44

---
+++ _dist_/index.js
@@ -0,0 +1,34 @@
+import { registerImage } from "./lazy.js";
+
+const randomNum = () => Math.floor(Math.random() * (122 - 1)) + 1;
+
+const addImageBtn = document.getElementById("get-image");
+const removeImageBtn = document.getElementById("delete-image");
+const deleteAllBtn = document.getElementById("delete-all");
+const imagesContainer = document.getElementById("images");
+
+const addNewImage = () => {
+  const image = document.createElement("img");
+  image.src =
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=";
+  image.dataset.src = `https://randomfox.ca/images/${randomNum()}.jpg`;
+  image.className = "mx-auto rounded-md bg-gray-300";
+  image.width = "320";
+
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("p-4");
+  imageContainer.append(image);
+
+  registerImage(imageContainer);
+  imagesContainer.append(imageContainer);
+};
+
+const removeLastImage = () => {
+  imagesContainer.lastChild
+    ? imagesContainer.lastChild.remove()
+    : alert("No hay más imagenes por eliminar");
+};
+
+addImageBtn.addEventListener("click", addNewImage);
+removeImageBtn.addEventListener("click", removeLastImage);
+deleteAllBtn.addEventListener("click", () => (imagesContainer.innerHTML = ""));
