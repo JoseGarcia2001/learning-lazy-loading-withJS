c10bc0afaa7b261474657079d1e5f8db2b81d1c0
Jose Garcia <jagarcia7655@gmail.com>
2021-05-21 19:07:01 -0500

    Eliminando comentarios

--- src/lazy.js
+++ src/lazy.js
@@ -1,29 +1,14 @@
-// let loadedImages = 0;
-// let renderImages = 0;
-
-// const logCount = () => {
-//   console.log(
-//     `
-//     ⚪ Se han agregado ${loadedImages} imágenes);
-//     🟣 Se han cargado ${renderImages} imágenes
-//     ------------------------------------------------
-//     `
-//   );
-// };
-
 import { count } from "./utils/manageCount";
 
 const loadImage = (entries) => {
   entries
     .filter((entrie) => entrie.isIntersecting)
     .forEach((entrie) => {
+      console.log(entrie);
       const image = entrie.target.firstChild;
       image.src = image.dataset.src;
       observer.unobserve(entrie.target);
       count.imageRendered();
-
-      // renderImages += 1;
-      // logCount();
     });
 };
 
@@ -32,7 +17,4 @@ const observer = new IntersectionObserver(loadImage);
 export const registerImage = (img) => {
   observer.observe(img);
   count.imageLoaded();
-
-  // loadedImages += 1;
-  // logCount();
 };
